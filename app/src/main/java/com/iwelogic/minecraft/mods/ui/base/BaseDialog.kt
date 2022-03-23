bc0847a4cdf33eb874e57ccb7967edcb037de8d2
Nazar <novaknazar@gmail.com>
2022-03-23 13:27:07 +0200

    filter design

--- app/src/main/java/com/iwelogic/minecraft/mods/ui/base/BaseDialog.kt
+++ app/src/main/java/com/iwelogic/minecraft/mods/ui/base/BaseDialog.kt
@@ -1,5 +1,7 @@
 package com.iwelogic.minecraft.mods.ui.base
 
+import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
@@ -11,6 +13,13 @@ open class BaseDialog<VM : BaseViewModel> : DialogFragment() {
         super.onStart()
         dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
         dialog?.window?.setBackgroundDrawable(null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.close.observe(viewLifecycleOwner) {
+           dismiss()
+        }
     }
 
     /*override fun showToast(msg: String?) {
