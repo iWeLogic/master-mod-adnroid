d6adbecd58d53890eb1091442b7eb4cfdf30e568
Nazar <novaknazar@gmail.com>
2022-03-26 12:10:34 +0200

    buttons style

--- app/src/main/java/com/iwelogic/minecraft/mods/bind/Details.kt
+++ app/src/main/java/com/iwelogic/minecraft/mods/bind/Details.kt
@@ -2,13 +2,12 @@ package com.iwelogic.minecraft.mods.bind
 
 import android.graphics.drawable.ClipDrawable
 import android.graphics.drawable.LayerDrawable
-import android.util.Log
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.databinding.BindingAdapter
 import com.iwelogic.minecraft.mods.R
+import com.iwelogic.minecraft.mods.models.Mod
 import com.iwelogic.minecraft.mods.ui.base.SliderAdapter
 import com.iwelogic.minecraft.mods.views.CollapseTextView
-import com.iwelogic.minecraft.mods.models.Mod
 import com.smarteist.autoimageslider.SliderView
 
 object Details {
@@ -34,7 +33,7 @@ object Details {
 
     @BindingAdapter("progress")
     @JvmStatic
-    fun setProgress(view: ImageView, progress: Int) {
+    fun setProgress(view: AppCompatImageButton, progress: Int) {
         val mClipDrawable = (view.background as LayerDrawable).findDrawableByLayerId(R.id.clip_drawable) as ClipDrawable
         mClipDrawable.level = progress
         view.isEnabled = progress != 0 || progress != 10000
