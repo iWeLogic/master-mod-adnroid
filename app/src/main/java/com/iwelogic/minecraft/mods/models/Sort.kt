5e080e78fa1550feefd01542fbb45b68f9f7388c
Nazar <novaknazar@gmail.com>
2022-04-04 12:04:34 +0300

    addDate sort

--- app/src/main/java/com/iwelogic/minecraft/mods/models/Sort.kt
+++ app/src/main/java/com/iwelogic/minecraft/mods/models/Sort.kt
@@ -7,7 +7,7 @@ enum class Sort(val id: String, val menuId: Int, val query: String) {
     LIKES("likes", R.id.sort_likes, "`likes` desc"),
     FILE_SIZE_DESC("file_desc", R.id.sort_file_desc, "`fileSize` desc"),
     FILE_SIZE_ASC("file_asc", R.id.sort_file_asc, "`fileSize` asc"),
-    DATE("date", R.id.sort_date, "`id` desc");
+    DATE("date", R.id.sort_date, "`addDate` desc");
 
     companion object {
         fun findByMenuId(id: Int) = values().firstOrNull { it.menuId == id } ?: DATE
