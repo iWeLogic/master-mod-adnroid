da3ce6ec41c08213725374cc84c4bd70805d4fdf
iwelogic <novaknazar@gmail.com>
2024-01-04 15:14:06 +0200

    fixed encouraging accidental clicks

--- app/src/main/java/com/iwelogic/minecraft/mods/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/iwelogic/minecraft/mods/ui/settings/SettingsViewModel.kt
@@ -1,6 +1,8 @@
 package com.iwelogic.minecraft.mods.ui.settings
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import com.google.android.ump.UserMessagingPlatform
 import com.iwelogic.minecraft.mods.ui.base.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -9,6 +11,7 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingsViewModel @Inject constructor(@ApplicationContext applicationContext: Context) : BaseViewModel(applicationContext) {
 
+    val isPrivacyPolicyVisible: MutableLiveData<Boolean> = MutableLiveData(UserMessagingPlatform.getConsentInformation(applicationContext).isConsentFormAvailable)
     val share: SingleLiveEvent<Boolean> = SingleLiveEvent()
     val rate: SingleLiveEvent<Boolean> = SingleLiveEvent()
     val openGDPR: SingleLiveEvent<Boolean> = SingleLiveEvent()
