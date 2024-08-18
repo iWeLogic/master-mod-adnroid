feb0a5e38b1c509173523cb9632d896694e93f72
Nazar_nno <nno@iagentur.ch>
2024-08-18 10:50:50 +0300

    up versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,26 +69,25 @@ dependencies {
 
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
+    implementation("androidx.fragment:fragment-ktx:1.8.2")
 
     //GoogleServices
-    implementation("com.google.android.gms:play-services-ads:23.0.0")
-    implementation("com.google.android.play:core:1.10.3")
+    implementation("com.google.android.gms:play-services-ads:23.3.0")
     implementation("com.google.android.play:core-ktx:1.8.1")
-    implementation("com.google.android.ump:user-messaging-platform:2.2.0")
+    implementation("com.google.android.ump:user-messaging-platform:3.0.0")
 
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-config")
     implementation("com.google.firebase:firebase-crashlytics")
@@ -101,8 +100,8 @@ dependencies {
     kapt("com.github.bumptech.glide:compiler:4.16.0")
 
     //DI Hilt
-    implementation("com.google.dagger:hilt-android:2.51")
-    kapt("com.google.dagger:hilt-android-compiler:2.51")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
     //Room
     implementation("androidx.room:room-runtime:2.6.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id ("com.android.application") version "8.3.0" apply false
     id ("com.android.library")version "8.3.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.8.22" apply false
-    id ("com.google.dagger.hilt.android") version "2.51" apply false
+    id ("com.google.dagger.hilt.android") version "2.51.1" apply false
     id ("com.google.firebase.crashlytics") version "2.9.6" apply false
     id ("com.google.gms.google-services") version "4.4.1" apply false
     id ("androidx.navigation.safeargs.kotlin" )version "2.7.7" apply false
