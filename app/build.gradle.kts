439b242e0696484079bf2368be5b85480b09e67c
Nazar_nno <nno@iagentur.ch>
2024-08-18 11:02:30 +0300

    change core library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
 
     //GoogleServices
     implementation("com.google.android.gms:play-services-ads:23.3.0")
-    implementation("com.google.android.play:core-ktx:1.8.1")
+    implementation("com.google.android.play:review-ktx:2.0.1")
     implementation("com.google.android.ump:user-messaging-platform:3.0.0")
 
     //Firebase
